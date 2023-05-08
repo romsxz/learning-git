a0c46b7a4df74c7fea42311dbdd8f81ecf938e2d
Romeo Magtibay <kyoheitakano1326@gmail.com>
2023-05-08 14:55:30 +0800

    Signed-off-by: Romeo Magtibay <kyoheitakano1326@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!123123");
+        System.out.println("Hello world1231231!123123123123");
     }
 }
